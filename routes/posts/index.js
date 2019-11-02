abc0287a1efca27f2bbcc03cd548f55b329bfa1f
choirakjun_ <choirak0805@gmail.com>
2019-11-02 18:18:41 +0900

    즐겨찾기 post/user 코드 작성.

--- routes/posts/index.js
+++ routes/posts/index.js
@@ -4,7 +4,6 @@ var multer = require('multer');
 var mysql = require('mysql');
 var config=require('../../config');
 var bodyParser = require('body-parser')
-var Q = require('Q');
 var path = require('path');
 
 // DATABASE SETTING
@@ -25,26 +24,120 @@ router.get('/process_due_posts', function (req, res){
 
 });
 
-};
-//SELECT * FROM download WHERE expiry_date > NOW()
 
-
-
-//input: post_id:xxx
-router.post('/close_post',function(req,res)=>{
+//input: post_id:xxx !!postid입력하면 해당 post삭제한다.
+router.post('/close_post',(req,res)=>{
 
 
   //
   id=req.body.id;
   var query = connection.query(`delete from Posts where id=?`,[id],(err,rows)=>{
-    if(err)res.json({"deleted:fail"});
+    if(err)res.json({"deleted":fail});
     else
     {
-        res.json({"deleted:success"});
+        res.json({"deleted":success});
     }
 
   });
 
 });
+
+
+
+
+//즐겨찾기 누를경우. 사용자가 선택한 post 테이블 선택
+//input==> {"postId":xx}
+router.post('/enroll_favorites',(req,res)=>{
+
+  user_id=req.user.id;
+  post_id=req.body.postId;
+
+
+  var query = connection.query(`insert into good(post_id,user_id) values ('${post_id}','${user_id}')`,(err,rows)=>{
+
+    if(err) {
+      console.error(err);
+      res.json({"result":"fail"});
+    }
+    else{
+      res.json({"result":"success"});
+    }
+
+  })
+})
+
+//즐겨찾기 다시 누를경우(즐겨찾기 해제)
+//input==> {"postId":xx}
+router.post('/cancel_favorites',(req,res)=>{
+
+//  user_id=req.user.id;
+  user_id=5;
+  post_id=req.body.postId;
+
+
+  var query = connection.query(`delete from good where user_id='${user_id}' and post_id='${post_id}'`,(err,rows)=>{
+
+    if(err) {
+      console.error(err);
+      res.json({"result":"fail"});
+    }
+    else{
+      res.json({"result":"success"});
+    }
+
+  })
+})
+
+
+
+//사용자가 누른 전체 즐겨찾기 postid 출력
+router.get('/user_favorites',async (req,res)=>{
+
+  user_id=req.user.id;
+  list_post_Id=[]
+
+  query = connection.query(`select post_id from good where user_id=${user_id}`,(err,rows)=>{
+
+    if(err) {
+      console.error(err);
+      res.json({"result":"fail"});
+    }
+    else{
+      console.log(rows);
+      list=[]
+      len = rows.length;
+      for(var i=0;i<len;i++)
+      {
+        //title startDate   endDate
+        post_Id=rows[i].post_id;
+        //console.log(post_Id);
+        list_post_Id[i]=post_Id;
+
+      }
+    }
+  })
+
+    //lsit_post_id에는 해당 사용자가 즐겨찾기한 postid 들의 리스트가 저장되어있음
+    //그리고 밑에서 리스트들을 돌면서 각 포스트의 title,startaate,enddate를 출력함
+  console.log("list_post_id",list_post_Id.length);
+      for (var i=0;i<list_post_Id.length;i++){
+          var query = connection.query(`select title,startDate,endDate  from posts where id=${list_post_Id[i]}`,(err,rows)=>{
+            console.log("시작");
+            title=rows[i].title;
+            startDate=rows[i].startDate;
+            endDate=rows[i].endDate;
+            list[i]=
+            {
+                "title":title,
+                "startDate":startDate,
+                "endDate":endDate
+            }
+          });
+
+          res.json(list);
+
+      }
+    });
+
 
 module.exports = router;
