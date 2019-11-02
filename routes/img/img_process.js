d409d9f81f3d96b7737782fe3886f30f3d567e37
choirakjun_ <choirak0805@gmail.com>
2019-11-02 13:32:02 +0900

    수정

--- routes/img/img_process.js
+++ routes/img/img_process.js
@@ -3,13 +3,123 @@ var router = express.Router();
 var multer = require('multer');
 var mysql = require('mysql');
 var config=require('../../config');
-var bodyParser = require('body-parser')
-var Q = require('Q');
+var bodyParser = require('body-parser');
 var path = require('path');
 
 // DATABASE SETTING
 var connection = mysql.createConnection(config);
 connection.connect()
+
+
+var upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, new Date().valueOf() + path.extname(file.originalname));
+    }
+  }),
+});
+
+
+/*
+form-data 형식
+key img1, img2, img3에 대해서 각각 이미지파일  받는다.
+*/
+//req.file로 업로드한 이미지가 저장된다.
+//또한 dest속성에 지정해둔 경로에 이미지가 저자에된다.
+//lomits 속성을 통해서 5MB로 파일 사이즈 제한 가능하다.
+//이미지가 아닌 데이터는 req.body에 저장된다.
+//const upload = multer({ dest: 'uploads/', limits: { fileSize: 5 * 1024 * 1024 } });
+
+//POST저장
+router.post('/up',  upload.fields([{ name: 'img1' }, { name: 'img2' }, { name: 'img3' }]), (req, res) => {
+
+
+  console.log(req.files);
+  img1_path=req.files.img1[0]['path'];
+  img2_path=req.files.img2[0]['path'];
+  img3_path=req.files.img3[0]['path'];
+
+  list=[];
+  list[0]=img1_path;
+  list[1]=img2_path;
+  list[2]=img2_path;
+  console.log(list);
+
+});
+
+
+//post수정
+router.post('/modify_post_img',  upload.fields([{ name: 'img1' }, { name: 'img2' }, { name: 'img3' },{name:'postid'}]), (req, res) => {
+
+  postid=req.body.postid;
+  img1_path=req.files.img1[0]['path'];
+  img2_path=req.files.img2[0]['path'];
+  img3_path=req.files.img3[0]['path'];
+
+  list=[];
+  list[0]=img1_path;
+  list[1]=img1_path;
+  list[2]=img2_path;
+  console.log(list);
+  console.log(postid);
+  res.end();
+});
+
+
+//postid에 해당하는 img_url 전체 return
+router.get('/:postid', function (req, res){
+
+
+		postid = req.params.postid;
+    var query = connection.query(`select * from posts where id=?`,[postid],(err,rows)=>{
+      if(err) console.error(err);
+
+      res.json(rows);
+
+    })
+
+});
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 전체 FLOW
@@ -18,9 +128,26 @@ connection.connect()
 Reference: https://www.learn2crack.com/2014/08/android-upload-image-node-js-server.html
 
 
+// router.post('/up',  upload.fields([{ name: 'postId' },{ name: 'img1' }, { name: 'img2' }, { name: 'img3' }]), (req, res) => {
+//   postid=req.body.postId;
+//
+//
+//
+//   console.log(req.files);
+//   res.end();
+// });
+
+
+
+
+
+
+
+
+
 */
 
-
+/*
 //input={postId:xxxx, post_img_num:1|2|3}
 router.post('/post_upload',function(req,res)=>{
 
@@ -130,13 +257,7 @@ router.get('/user_profile', function (req, res){
 
 });
 };
-
-
-module.exports = router;
-
-
-
-
+*/
 
 
 
